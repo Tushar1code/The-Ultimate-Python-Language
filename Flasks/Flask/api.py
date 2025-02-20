6fe520ef4cf2fc4c7fdce5d65964b334049f5dc9
Tushar1code <tusharambatwar@gmail.com>
2025-02-20 21:10:21 +0530

    Your commit message

--- Flasks/Flask/api.py
+++ Flasks/Flask/api.py
@@ -37,5 +37,16 @@ def create_item():
     items.append(new_item)
     return jsonify(new_item)
 
+##update an exisiting item
+@app.route('/items/<int:item_id>',methods=['PUT'])
+def update_item(item_id):
+    item = next((item for item in items if item["id"] == item_id),None)
+    if item is None:
+        return jsonify({"error":"Item not found"})
+    item['name'] = request.json.get('name',item['item'])
+    item['description'] = request.json.get('description',item['description'])
+    return jsonify(item)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
